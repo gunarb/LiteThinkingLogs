43cc8b80e4d4fdbb55cd194d0fb85b86a728e1b6
Gunnar Bolanos <gbolanos16@gmail.com>
2024-10-05 19:40:14 -0600

    Logs App

---
+++ Domain/LogEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain;
+
+public class LogEntity
+{
+    public Guid LogId { get; set; }
+
+    public required string Type { get; set; }
+    
+    public string? Description { get; set; }
+    
+    public DateTime CreateDate { get; set; }
+}
